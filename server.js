ac741968be7c952c4e8fc05ef7218901f9f81507
Ahmad Hamid <ahmad.hamid599@outlook.com>
2022-07-24 23:14:49 +0500

    done with pusher intigration

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+  appId: "1441964",
+  key: "3837ddb7118dbb2fa7f5",
+  secret: "e2eb2d585f67ff948a6e",
+  cluster: "ap2",
+  useTLS: true,
+});
+
 //middleware
+app.use(express.json());
 
 //DB config
 const connection_url =
@@ -21,7 +31,18 @@ mongoose.connect(connection_url);
 app.get("/", (req, res) =>
   res.status(200).send("lets goo!༼ つ ◕_◕ ༽つ(╯°□°）╯︵ ┻━┻")
 );
-app.post("/api/v1/messages/new", (req, res) => {
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
   Messages.create(dbMessage, (err, data) => {
